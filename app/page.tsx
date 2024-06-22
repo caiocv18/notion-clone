79b20bba5e24ddbb1030d66c2ef5f02ff7e4bb22
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-06-22 01:35:28 -0300

    style: Update page content in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello Notion Clone</p>
+    <p className="text-red-500 font-bold">Hello Notion Clone</p>
   );
 }
